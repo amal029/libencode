b41270d359a9afff7c1fcd348a554d60c477639d
Avinash Malik <avinash.malik@auckland.ac.nz>
2015-04-06 16:41:53 +1200

    Added support for producing sexpressions from bencode formats

--- util.scm
+++ util.scm
@@ -2,14 +2,16 @@
 
 ;;; parition by index function
 (define (parition-indexed f l)
-  (letrec ((part (lambda (count fi se)
-		   (if (null? l)
+  (letrec ((part (lambda (count fi se ll)
+		   (if (null? ll)
 		       (values fi se)
 		       (cond
-			((f (car l) count)
+			((f (car ll) count)
 			 (part (+ count 1)
-			       (cons (car l) fi) se))
+			       (cons (car l) fi) se
+			       (cdr ll)))
 			(else
 			 (part (+ count 1) fi
-			       (cons (car l) se))))))))
-    (part '() '() 0)))
+			       (cons (car l) se)
+			       (cdr ll))))))))
+    (part '() '() 0 l)))
