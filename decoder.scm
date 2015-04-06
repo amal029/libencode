07d27431f4051c9535ce68aacff527a3ca4bb0f8
Avinash Malik <avinash.malik@auckland.ac.nz>
2015-04-06 13:00:13 +1200

    Need to make function to create alist from list, rest of the decoder should be ok

--- decoder.scm
+++ decoder.scm
@@ -1,5 +1,62 @@
-(declare (unit decoder) (uses extras))
+(declare
+  (hide build-string-type)
+  (hide build-int-type)
+  (hide build-list-type)
+  (hide build-dict-type)
+  (unit decoder) (uses extras))
 
+;;; Stops reading after the specified number of characters
+(define (build-string-type iport chars)
+  (letrec ((build (lambda (res count)
+		    (let ((c (read-char iport)))
+		      (if (<= count (+ 1 chars))
+			  (cond
+			   ((eof-object? c)
+			    (error "Malformed Bencode string:" res))
+			   ((equal? count 0)
+			    (build res (+ 1 count)))
+			   (else
+			    (build (cons c res) (+ 1 count))))
+			  (list->string res))))))
+    (build '() 0)))
+
+;;; Uses the delimeter #\e to stop reading
+(define (build-int-type iport)
+  (letrec ((build (lambda (res)
+		    (let ((c (read-char iport)))
+		      (cond
+		       ((eof-object? c)
+			(error "Malformed Bencode integer: " res))
+		       ((equal? #\e c)
+			(string->number (list->string res)))
+		       (else (build (cons c res))))))))
+    (build '())))
+
+;;; Uses the delimeter #\e to stop reading
+(define (build-list-type iport)
+  (letrec ((build (lambda (res)
+		    (let ((c (read-char iport)))
+		      (cond
+		       ((eof-object? c)
+			(error "Malformed Bencode list: " res))
+		       ((equal? #\e c) res)
+		       (else
+			(build (cons (decoder iport) res))))))))
+    (build '())))
+
+(define (build-dict-type iport)
+  '())
 
 (define (decoder iport)
-  )
+  (let ((c (read-char iport)))
+    (cond
+     ;; I represent the EOF with a '()
+     ((eof-object? c)
+      (error "Malformed Bencode: " c))
+     (else
+      (cond
+       ((equal? #\i c) (build-int-type  iport))
+       ((equal? #\l c) (build-list-type iport))
+       ((equal? #\d c) (build-dict-type iport))
+       ((char-numeric? c) (build-string-type
+			   iport (char->integer c))))))))
